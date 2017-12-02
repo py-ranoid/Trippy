a6dec64a0aab8836801dffc2581e08edcd2c3698
Oofone <shreyas.gopal.ind@gmail.com>
2017-12-02 10:25:21 +0530

    Started the Bing Scraper

---
+++ TrippyMain/Warehouse/bing_image.py
@@ -0,0 +1,8 @@
+import json
+import requests
+from pprint import pprint
+Key1 = "82ff9ab8a7614161b2bba97dfbbce3f5"
+Key2 = "f94ad171570449278a28c720c9c76235"
+
+subscriptionKey = Key1
+endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
